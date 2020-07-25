036935fc029a0b81740462eddba96e40e405d274
lizlove <elizabethlovero@gmail.com>
2020-07-25 11:31:53 -0400

    Basic user model creation.

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 const url =
-  "mongodb+srv://admin:<password>@nightlyapi.22ja7.mongodb.net/<dbname>?retryWrites=true&w=majority";
+  "mongodb+srv://admin:nightly@nightlyapi.22ja7.mongodb.net/nightly?retryWrites=true&w=majority";
 
-mongoose.connect(url, { useNewUrlParser: true });
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once("open", () =>
   console.log(`Connected to mongo at ${url}`)
 );
